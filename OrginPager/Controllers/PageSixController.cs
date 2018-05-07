3ccd233d7bf613590e468a50e6db9d051493dd58
少年那已不再 <15997352948@163.com>
2018-05-07 20:23:22 +0800

    添加一个基本PagedList分页

--- OrginPager/Controllers/PageSixController.cs
+++ OrginPager/Controllers/PageSixController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace OrginPager.Controllers
 {
     public class PageSixController : Controller
     {
-        // GET: PageFour
+        // GET: PageSix
         public ActionResult Index(int? id)
         {
             //具体的页面数 
@@ -20,6 +21,19 @@ namespace OrginPager.Controllers
             List<Models.Person> list = new OrginPager.BLL.PersonService().GetAllPerson(pageSize, pageIndex, "", out totalCount);
             ViewBag.totalCount = totalCount;
             return View(list);
+        }
+
+        public ActionResult List(int? page)
+        {
+            //具体的页面数 
+            int pageIndex = page ?? 1;
+            //页面显示条数
+            int pageSize = Request["pageSize"] == null ? 10 : Convert.ToInt32(Request["pageSize"]);
+            // 数据记录条数
+            int totalCount = 0;
+            List<Models.Person> list = new OrginPager.BLL.PersonService().GetAllPerson(pageSize, pageIndex, "", out totalCount);
+            var personsAsIPagedList = new StaticPagedList<Models.Person>(list, pageIndex, pageSize, totalCount);
+            return View(personsAsIPagedList);
         }
     }
 }
