567c242589469da1e90a1b782e9b3858df7dc2e8
少年那已不再 <15997352948@163.com>
2018-05-08 16:40:06 +0800

    整理了分页案例顺序，添加了KKPager静态分页

---
+++ OrginPager/Controllers/PageFourController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrginPager.Controllers
+{
+    public class PageFourController : Controller
+    {
+        // GET: PageFour
+        private readonly int pageSize = 10;
+
+        public ActionResult Index()
+        {
+            //页面显示条数
+            ViewBag.PageSize = pageSize;
+            // 数据记录条数
+            ViewBag.TotalCount = new OrginPager.BLL.PersonService().GetPersonCount();
+            return View();
+        }
+
+        public ActionResult AjaxPaging(int pageIndex = 1)
+        {
+            int totalCount = 0;
+            var persons = new OrginPager.BLL.PersonService().GetAllPerson(pageSize, pageIndex, "", out totalCount);
+            return PartialView("_Index", persons);
+        }
+    }
+}
